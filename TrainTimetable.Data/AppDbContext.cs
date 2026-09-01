b54296d72915954418b74f7551721ded6f9659ba
SNstat <simun.cosic@gmail.com>
2026-09-01 16:19:56 +0200

    Added sync seeding of development data during OnConfiguring.

--- TrainTimetable.Data/AppDbContext.cs
+++ TrainTimetable.Data/AppDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TrainTimetable.Data.Entities;
+using TrainTimetable.Data.Seeds;
 
 namespace TrainTimetable.Data;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<TrainManufacturer> TrainManufacturers { get; set; }
     public DbSet<Train> Trains { get; set; }
     public DbSet<Station> Stations { get; set; }
     public DbSet<Country> Countries { get; set; }
@@ -28,6 +30,9 @@ public class AppDbContext : DbContext
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TrainTimetable;Integrated Security=True");
         }
 
+        optionsBuilder
+            .UseSeeding(async (dbContext, _) => DataSeeder.SeedDevelopmentData(dbContext));
+
         base.OnConfiguring(optionsBuilder);
     }
 
@@ -36,60 +41,21 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<TrainManufacturer>(_ =>
         {
             _.HasIndex(_ => _.Name).IsUnique();
-            _.HasData(
-                new TrainManufacturer { ID = 1, Name = "FS Trenitalia" },
-                new TrainManufacturer { ID = 2, Name = "Bombardier Transportation" },
-                new TrainManufacturer { ID = 3, Name = "Alstom" });
-        });
-
-        modelBuilder.Entity<Train>(_ => {
-            _.HasData(
-                new Train { ID = 1, TrainManufacturerID = 1, Name = "E.403", SeatCount = 60 },
-                new Train { ID = 2, TrainManufacturerID = 2, Name = "S Stock", SeatCount = 50 },
-                new Train { ID = 3, TrainManufacturerID = 3, Name = "X65", SeatCount = 76 });
         });
 
         modelBuilder.Entity<Country>(_ =>
         {
             _.HasIndex(_ => _.Name).IsUnique();
-            _.HasData(
-                new Country { ID = 1, Name = "Croatia"}
-                );
         });
 
         modelBuilder.Entity<Station>(_ => {
             _.HasIndex(_ => _.Name).IsUnique();
-            _.HasData(
-                new Station { ID = 1, Name = "Varaždin", BaseStationID = null, CountryID = 1 },
-                new Station { ID = 2, Name = "Turčin", BaseStationID = 1, CountryID = 1 },
-                new Station { ID = 3, Name = "Doljan", BaseStationID = 1, CountryID = 1 },
-                new Station { ID = 4, Name = "Krušljevec", BaseStationID = 1, CountryID = 1 },
-                new Station { ID = 5, Name = "Čakovec", BaseStationID = 1, CountryID = 1 });
-        });
-
-        modelBuilder.Entity<Line>(_ =>
-        {
-            _.HasData(
-                new Line { ID = 1, LineNumber = 1, TrainID = 1 },
-                new Line { ID = 2, LineNumber = 2, TrainID = 2 }
-                );
         });
 
         modelBuilder.Entity<Stop>(_ =>
         {
             _.HasIndex(x => new { x.LineID, x.Order }).IsUnique();
             _.HasIndex(x => new { x.LineID, x.StationID }).IsUnique();
-
-            _.HasData(
-                new Stop { ID = 1, ArrivalTime = null, DepartureTime = new TimeOnly(10, 35, 0), StationID = 4, LineID = 1, Order = 1 },
-                new Stop { ID = 2, ArrivalTime = new TimeOnly(10, 50, 0), DepartureTime = new TimeOnly(10, 55, 0), StationID = 3, LineID = 1, Order = 2 },
-                new Stop { ID = 3, ArrivalTime = new TimeOnly(11, 10, 0), DepartureTime = new TimeOnly(11, 15, 0), StationID = 2, LineID = 1, Order = 3 },
-                new Stop { ID = 4, ArrivalTime = new TimeOnly(11, 30, 0), DepartureTime = null, StationID = 1, LineID = 1, Order = 4 },
-
-                new Stop { ID = 5, ArrivalTime = null, DepartureTime = new TimeOnly(12, 35, 0), StationID = 3, LineID = 2, Order = 1 },
-                new Stop { ID = 6, ArrivalTime = new TimeOnly(12, 50, 0), DepartureTime = new TimeOnly(12, 55, 0), StationID = 1, LineID = 2, Order = 2 },
-                new Stop { ID = 7, ArrivalTime = new TimeOnly(13, 10, 0), DepartureTime = null, StationID = 5, LineID = 2, Order = 3 }
-                );
         });
 
         base.OnModelCreating(modelBuilder);
---
+++ TrainTimetable.Data/Seeds/DataSeeder.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainTimetable.Data.Entities;
+
+namespace TrainTimetable.Data.Seeds;
+
+internal static class DataSeeder
+{
+    internal static void SeedDevelopmentData(DbContext _dbContext)
+    {
+        //Country
+        var countries = new List<Country>()
+        {
+            new() { Name = "Croatia" }
+        };
+
+        _dbContext.Set<Country>().AddRange(countries);
+
+        //Station
+        var stations = new List<Station>()
+        {
+            new() { Name = "Varaždin", BaseStationID = null, Country = countries[0] },
+            new() { Name = "Turčin", BaseStationID = 1, Country = countries[0] },
+            new() { Name = "Doljan", BaseStationID = 1, Country = countries[0] },
+            new() { Name = "Krušljevec", BaseStationID = 1, Country = countries[0] },
+            new() { Name = "Čakovec", BaseStationID = 1, Country = countries[0] }
+        };
+
+        _dbContext.Set<Station>().AddRange(stations);
+
+        //TrainManufacturer
+        var trainManufacturers = new List<TrainManufacturer>()
+        {
+            new() { Name = "FS Trenitalia" },
+            new() { Name = "Bombardier Transportation" },
+            new() { Name = "Alstom" }
+        };
+
+        _dbContext.Set<TrainManufacturer>().AddRange(trainManufacturers);
+
+        //Train
+        var trains = new List<Train>()
+        {
+            new() { TrainManufacturer = trainManufacturers[0], Name = "E.403", SeatCount = 60 },
+            new() { TrainManufacturer = trainManufacturers[1], Name = "S Stock", SeatCount = 50 },
+            new() { TrainManufacturer = trainManufacturers[2], Name = "X65", SeatCount = 76 }
+        };
+
+        _dbContext.Set<Train>().AddRange(trains);
+
+        //Line
+        var lines = new List<Line>()
+        {
+            new() { LineNumber = 1, Train = trains[0] },
+            new() { LineNumber = 2, Train = trains[1] }
+        };
+
+        _dbContext.Set<Line>().AddRange(lines);
+
+        //Stop
+        var stops = new List<Stop>()
+        {
+            new Stop { ArrivalTime = null, DepartureTime = new TimeOnly(10, 35, 0), Station = stations[3], Line = lines[0], Order = 1 },
+            new Stop { ArrivalTime = new TimeOnly(10, 50, 0), DepartureTime = new TimeOnly(10, 55, 0), Station = stations[2], Line = lines[0], Order = 2 },
+            new Stop { ArrivalTime = new TimeOnly(11, 10, 0), DepartureTime = new TimeOnly(11, 15, 0), Station = stations[1], Line = lines[0], Order = 3 },
+            new Stop { ArrivalTime = new TimeOnly(11, 30, 0), DepartureTime = null, Station = stations[0], Line = lines[0], Order = 4 },
+
+            new Stop { ArrivalTime = null, DepartureTime = new TimeOnly(12, 35, 0), Station = stations[1], Line = lines[1], Order = 1 },
+            new Stop { ArrivalTime = new TimeOnly(12, 50, 0), DepartureTime = new TimeOnly(12, 55, 0), Station = stations[0], Line = lines[1], Order = 2 },
+            new Stop { ArrivalTime = new TimeOnly(13, 10, 0), DepartureTime = null, Station = stations[4], Line = lines[1], Order = 3 }
+        };
+
+        _dbContext.Set<Stop>().AddRange(stops);
+
+        _dbContext.SaveChanges();
+    }
+}
