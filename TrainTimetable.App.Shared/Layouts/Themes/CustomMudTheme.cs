736efb5d51d70ff00063e554b4323740df8e5e86
SNstat <simun.cosic@gmail.com>
2026-09-10 15:19:27 +0200

    Added custom theming.

---
+++ TrainTimetable.App.Shared/Layouts/Themes/CustomMudTheme.cs
@@ -0,0 +1,26 @@
+﻿using MudBlazor;
+
+namespace TrainTimetable.App.Shared.Layouts.Themes;
+
+public class CustomMudTheme : MudTheme
+{
+    public CustomMudTheme()
+    {
+        PaletteLight = new PaletteLight()
+        {
+            Primary = "#1d7874",
+            Secondary = "#ee2e31",
+            AppbarBackground = "#071e22",
+            Background = "#eeeeee",
+            Surface = "#ffffff",
+        };
+
+        Typography = new Typography()
+        {
+            Default = new DefaultTypography
+            {
+                FontFamily = new[] { "Poppins", "sans-serif" }
+            }
+        };
+    }
+}
