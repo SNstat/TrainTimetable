b57d78d16bb8a9e4a07aa7adf567f29873acf905
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-09-14 09:47:29 +0200

    evo bibi

--- TrainTimetable.App/Program.cs
+++ TrainTimetable.App/Program.cs
@@ -29,7 +29,6 @@ public class Program
             app.UseHsts();
         }
 
-        app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
         app.UseHttpsRedirection();
 
         app.UseAntiforgery();
